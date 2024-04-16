066970ce27c850cb3e1632cb9bbe6f031440bc8b
Cephas-Muhia <Cephaschronixx@gmail.com>
2024-04-16 19:10:11 +0300

    test cases

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,46 @@
 #!/usr/bin/python3
-""" """
+
+"""
+Test module for User model
+"""
 from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
+from models.user import User
+import os
 
 
-class test_review(test_basemodel):
+class test_User(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        self.name = "User"
+        self.value = User
 
-    def test_place_id(self):
+    def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertEqual(type(new.first_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def test_user_id(self):
+    def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(type(new.last_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def test_text(self):
+    def test_email(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(new.email), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_password(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.password), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
