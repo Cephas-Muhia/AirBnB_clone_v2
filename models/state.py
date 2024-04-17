5171cf53c20733af067a861b8ad6d422bfeee1a8
Matthew Mwangi <144494969+thigam@users.noreply.github.com>
2024-04-17 22:02:44 +0200

    Update state.py restored it to original version

--- models/state.py
+++ models/state.py
@@ -6,11 +6,3 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        print(kwargs)
-        """super().__init__(**kwargs)"""
-        """self.name = kwargs["name"]"""
-        if (kwargs["name"]):
-            setattr(self, 'name', kwargs["name"])
