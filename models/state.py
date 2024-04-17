001f8c02b1ea7f30cf8f5c99622c7d05cb20abc7
Matthew Mwangi <144494969+thigam@users.noreply.github.com>
2024-04-17 21:48:13 +0200

    Update state.py to accept arguments when initializing

--- models/state.py
+++ models/state.py
@@ -6,3 +6,11 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        print(kwargs)
+        """super().__init__(**kwargs)"""
+        """self.name = kwargs["name"]"""
+        if (kwargs["name"]):
+            setattr(self, 'name', kwargs["name"])
